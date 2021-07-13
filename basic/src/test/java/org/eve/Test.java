445cbc884bbc8f57bab5bcf3742ddecfe6c8ccb5
huangfen <huangfen@kuaishou.com>
2021-07-13 15:42:07 +0800

    add test

---
+++ basic/src/test/java/org/eve/Test.java
@@ -0,0 +1,12 @@
+package org.eve;
+
+/**
+ * @author EvelynHf
+ * @Date 2021/07/13 15:40:00
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.printf("hello world!");
+    }
+}
---
+++ basic/src/test/java/org/eve/singleton/LazyOneTest.java
@@ -0,0 +1,10 @@
+package org.eve.singleton;
+
+/**
+ * @author EvelynHf
+ * @Date 2021/07/13 15:41:00
+ */
+public class LazyOneTest {
+    public static void main(String[] args) {
+    }
+}
