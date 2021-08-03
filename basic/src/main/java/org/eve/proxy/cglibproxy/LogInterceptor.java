ff0075df6d4c4d8e8784354b496568800fd0a124
huangfen132 <huangfen132@163.com>
2021-08-03 20:19:29 +0800

    proxy

--- basic/src/main/java/org/eve/proxy/cglibproxy/LogInterceptor.java
+++ basic/src/main/java/org/eve/proxy/cglibproxy/LogInterceptor.java
@@ -2,7 +2,6 @@ package org.eve.proxy.cglibproxy;
 
 import java.lang.reflect.Method;
 
-import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
@@ -11,16 +10,6 @@ import net.sf.cglib.proxy.MethodProxy;
  * @Date 2021/08/03 18:44:00
  */
 public class LogInterceptor implements MethodInterceptor {
-
-    private Enhancer enhancer = new Enhancer();
-
-    public Object getProxy(Class clazz) {
-        // 设置需要创建子类的类
-        enhancer.setSuperclass(clazz);
-        enhancer.setCallback(this);
-        // 通过字节码技术动态创建子类实例
-        return enhancer.create();
-    }
 
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         System.out.printf("before invoke!\n");
